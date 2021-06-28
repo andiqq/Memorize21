81204dd7906a838326ca55b7d082ec40b6d9f5a5
Dr. Andreas Plagens <andreas@plagens.eu>
2021-06-28 20:24:42 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Dr. Andreas Plagens on 28.06.21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
